340d52f0f8b91df418bedee45b1d1369f276a40b
Enzoprt <134990663+mindviper@users.noreply.github.com>
2024-06-09 03:34:39 -0300

    Same code for IR remote but using switch case
    Switch case for blazingly fast performance

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <msp430f5529.h>
 
-//
+// Define an enum for button types
+typedef enum {
+    RED_LED_ON = 0x00FFA25D,
+    RED_LED_OFF = 0x00FF629D,
+    RED_LED_TOGGLE = 0x00FFE21D,
+    GREEN_LED_ON = 0x00FF22DD,
+    GREEN_LED_OFF = 0x00FF02FD,
+    GREEN_LED_TOGGLE = 0x00FFC23D,
+    MODE_CHANGE = 0x00FF6897,
+    PWM_INC = 0x00FF18E7,
+    PWM_DEC = 0x00FF4AB5
+} ButtonType;
+
 #define TARGET_INTERVAL 14051  // 1 count = 1us
 #define DEBOUNCE_INTERVAL 90 // .1ms debounce interval to compensate for slight hardware deviations
-#define high_interval 2360 //2.2ms interval between RISING edge means logic 1
-#define low_interval 1237 //1.18ms interval between RISING edge means logic 0
-#define smalldebounceinterval 50 //
+#define HIGH_INTERVAL 2360 //2.2ms interval between RISING edge means logic 1
+#define LOW_INTERVAL 1237 //1.18ms interval between RISING edge means logic 0
+#define SMALL_DEBOUNCE_INTERVAL 50 //
 #define TRUE 1
 #define FALSE 0
 
@@ -15,16 +27,7 @@ unsigned int currentCapture = 0;
 unsigned int lastCapture = 0; // Stores the last capture value
 unsigned int nbits = 32; //number of bits to be captured
 unsigned long packet = 0;
-unsigned long red_ledON = 0x00FFA25D; //value for red led to turn on
-unsigned long red_ledOFF = 0x00FF629D;
-unsigned long red_ledTOGGLE = 0x00FFE21D;
-unsigned long green_ledON = 0x00FF22DD;
-unsigned long green_ledOFF = 0x00FF02FD;
-unsigned long green_ledTOGGLE = 0x00FFC23D;
-unsigned long mode_change = 0x00FF6897; //asterisk key
-unsigned long pwm_inc = 0x00FF18E7;
-unsigned long pwm_dec = 0x00FF4AB5;
-volatile char mode= 0;
+volatile char mode = 0;
 int dutycycle = 5000;
 volatile char partok = FALSE;
 
@@ -37,65 +40,13 @@ void intrestore();
 void effect();
 void PWM_timer_setup();
 void PWM_timer_undo();
+void Init();
 
-
-
-/**
- * IMPORTANTE
- *
- * Este projeto utiliza um controle ligeiramente diferente do proposto na proposta do visto 2
- * Como consequência, tive que fazer algumas alterações em sua funcionalidade.
- * Diferentemente do pedido na proposta, ao invés de utilizar as teclas numeradas para escolher
- * o ciclo de carga em incrementos de 10% por tecla, utilizei as setas para incrementar e decrementar o ciclo de carga
- * em incrementos de 10%. Ao pressionar a tecla da seta para cima, o ciclo de carga é aumentado em 10% e reduzido em 10%
- * quando pressionada a tecla da seta para baixo. Abaixo segue um resumo das funcionalidades das teclas:
- *
- * MODO 0:
- *      1: Liga led vermelho
- *      2: Desliga led vermelho
- *      3: Inverte led vermelho
- *      4: Liga led verde
- *      5: Desliga led verde
- *      6: Inverte led verde
- *      *: Altera Modo de operação para o MODO 1
- *
- *
- * MODO 1:
- *      1: Liga led vermelho
- *      2: Desliga led vermelho
- *      3: Inverte led vermelho
- *      4: 100% de ciclo de carga da PWM do led verde
- *      5: 0% de ciclo de carga da PWM do led verde
- *      6: Inverte ciclo de carga da PWM do led verde led verde
- *      UP: Incerementa em 10% o ciclo de carga da PWM do led verde (até  100%)
- *      DOWN: Recuz em 10% o ciclo de carga da PWM do led verde (até 0%)
- *      *: Altera Modo de operação para o MODO 0
- *
- */
-
-
-// Initialize the system
 void main(void) {
-    // Stop watchdog timer
-    WDTCTL = WDTPW | WDTHOLD;
 
-    // Set Timer A0 to use SMCLK, continuous mode
-    TA0CTL = TASSEL_2 | MC_2;
+    Init(); // Initialize
 
-    // Set P1.0 as output (red LED)
-    P1DIR |= BIT0;
-    P1OUT &= ~BIT0; // Initialize P1.0 to low (LED off)
-
-    // Set P4.7 as output (green LED)
-    P4DIR |= BIT7;
-    P4OUT &= ~BIT7; // Initialize P4.7 to low
-
-    // Set P2.0 as input with pull-up resistor
-    P2DIR &= ~BIT0;
-    P2REN |= BIT0;
-    P2OUT |= BIT0;
-
-    setupINT();
+    setupINT(); // Sets up interrupts
 
     _enable_interrupt();
 
@@ -118,11 +69,11 @@ __interrupt void PORT2_ISR(void) {
             partok = TRUE;
         }
     } else {
-        if (interval >= (high_interval - DEBOUNCE_INTERVAL) && interval <= (high_interval + DEBOUNCE_INTERVAL)){
+        if (interval >= (HIGH_INTERVAL - DEBOUNCE_INTERVAL) && interval <= (HIGH_INTERVAL + DEBOUNCE_INTERVAL)){
             nbits--;
             writeone();
         }
-        if (interval >= (low_interval - DEBOUNCE_INTERVAL) && interval <= (low_interval + DEBOUNCE_INTERVAL)){
+        if (interval >= (LOW_INTERVAL - DEBOUNCE_INTERVAL) && interval <= (LOW_INTERVAL + DEBOUNCE_INTERVAL)){
             nbits--;
             writezero();
         }
@@ -139,6 +90,29 @@ __interrupt void PORT2_ISR(void) {
 
     // Clear interrupt flag for P2.0
     P2IFG &= ~BIT0;
+}
+
+void Init(){
+
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;
+
+    // Set Timer A0 to use SMCLK, continuous mode
+    TA0CTL = TASSEL_2 | MC_2;
+
+    // Set P1.0 as output (red LED)
+    P1DIR |= BIT0;
+    P1OUT &= ~BIT0; // Initialize P1.0 to low (LED off)
+
+    // Set P4.7 as output (green LED)
+    P4DIR |= BIT7;
+    P4OUT &= ~BIT7; // Initialize P4.7 to low
+
+    // Set P2.0 as input with pull-up resistor
+    P2DIR &= ~BIT0;
+    P2REN |= BIT0;
+    P2OUT |= BIT0;
+
 }
 
 void setupINT() {
@@ -167,71 +141,80 @@ void intrestore(){
 }
 
 void effect(){
-    if(mode == 0){
-        if (packet == red_ledON){
-            P1OUT |= BIT0; // Turn on red LED
-        }
-        if (packet == red_ledOFF){
-            P1OUT &= ~BIT0; // Turn off red LED
-        }
-        if (packet == red_ledTOGGLE){
-            P1OUT ^= BIT0; // Toggle red LED
-        }
-        if (packet == green_ledON){
-            P4OUT |= BIT7; // Turn on green LED
-        }
-        if (packet == green_ledOFF){
-            P4OUT &= ~BIT7; // Turn off green LED
-        }
-        if (packet == green_ledTOGGLE){
-            P4OUT ^= BIT7; // Toggle green LED
-        }
-        if (packet == mode_change){
-            mode = 1;
-            PWM_timer_setup();
-        }
-    }
-
-    else { // PWM mode
-        if (packet == red_ledON){
-            P1OUT |= BIT0; // Turn on red LED
-        }
-        if (packet == red_ledOFF){
-            P1OUT &= ~BIT0; // Turn off red LED
-        }
-        if (packet == red_ledTOGGLE){
-            P1OUT ^= BIT0; // Toggle red LED
-        }
-        if (packet == green_ledON){
-            dutycycle = 10000; // Set duty cycle to 100% (completely on)
-            TBCCR1 = dutycycle;
-        }
-        if (packet == green_ledOFF){
-            dutycycle = 0; // Set duty cycle to 0%  (completely off)
-            TBCCR1 = dutycycle;
-        }
-        if (packet == green_ledTOGGLE){
-            dutycycle = 10000 - dutycycle; // Toggle green LED
-            TBCCR1 = dutycycle;
-        }
-        if (packet == pwm_inc){
-            dutycycle += 1000;
-            if (dutycycle >= 10000){
-                dutycycle = 10000; // Prevent duty cycle from exceeding 100%
+    switch (mode) {
+        case 0:
+            switch ((ButtonType)packet) {
+                case RED_LED_ON:
+                    P1OUT |= BIT0; // Turn on red LED
+                    break;
+                case RED_LED_OFF:
+                    P1OUT &= ~BIT0; // Turn off red LED
+                    break;
+                case RED_LED_TOGGLE:
+                    P1OUT ^= BIT0; // Toggle red LED
+                    break;
+                case GREEN_LED_ON:
+                    P4OUT |= BIT7; // Turn on green LED
+                    break;
+                case GREEN_LED_OFF:
+                    P4OUT &= ~BIT7; // Turn off green LED
+                    break;
+                case GREEN_LED_TOGGLE:
+                    P4OUT ^= BIT7; // Toggle green LED
+                    break;
+                case MODE_CHANGE:
+                    mode = 1;
+                    PWM_timer_setup();
+                    break;
+                default:
+                    break;
             }
-            TBCCR1 = dutycycle;
-        }
-        if (packet == pwm_dec){
-            dutycycle -= 1000;
-            if (dutycycle <= 0){
-                dutycycle = 0; // Prevent duty cycle from going below 0%
+            break;
+        case 1: // PWM mode
+            switch ((ButtonType)packet) {
+                case RED_LED_ON:
+                    P1OUT |= BIT0; // Turn on red LED
+                    break;
+                case RED_LED_OFF:
+                    P1OUT &= ~BIT0; // Turn off red LED
+                    break;
+                case RED_LED_TOGGLE:
+                    P1OUT ^= BIT0; // Toggle red LED
+                    break;
+                case GREEN_LED_ON:
+                    dutycycle = 10000; // Set duty cycle to 100% (completely on)
+                    TBCCR1 = dutycycle;
+                    break;
+                case GREEN_LED_OFF:
+                    dutycycle = 0; // Set duty cycle to 0%  (completely off)
+                    TBCCR1 = dutycycle;
+                    break;
+                case GREEN_LED_TOGGLE:
+                    dutycycle = 10000 - dutycycle; // Toggle green LED
+                    TBCCR1 = dutycycle;
+                    break;
+                case PWM_INC:
+                    dutycycle += 1000;
+                    if (dutycycle >= 10000){
+                        dutycycle = 10000; // Prevent duty cycle from exceeding 100%
+                    }
+                    TBCCR1 = dutycycle;
+                    break;
+                case PWM_DEC:
+                    dutycycle -= 1000;
+                    if (dutycycle <= 0){
+                        dutycycle = 0; // Prevent duty cycle from going below 0%
+                    }
+                    TBCCR1 = dutycycle;
+                    break;
+                case MODE_CHANGE:
+                    mode = 0;
+                    PWM_timer_undo(); // Turns off timer and unmaps TB0.1 from P4.7
+                    break;
+                default:
+                    break;
             }
-            TBCCR1 = dutycycle;
-        }
-        if (packet == mode_change){
-            mode = 0;
-            PWM_timer_undo();
-        }
+            break;
     }
 }
 
@@ -240,29 +223,21 @@ void PWM_timer_setup(){
     P4DIR |= BIT7;
     P4SEL |= BIT7;
 
-    // Enable write access to port mapping registers
     PMAPKEYID = PMAPKEY;
     P4MAP7 = PM_TB0CCR1A; // Map TB0.1 to P4.7
-    PMAPKEYID = 0; // Disable write access to port mapping registers
+    PMAPKEYID = 0;
 
-    // Set Timer B0 to use SMCLK, up mode
+    // Timer setup
     TBCTL = TBSSEL__SMCLK | MC__UP | TBCLR;
-
-    // Set the period of the PWM signal
-    TBCCR0 = 10000;
-
-    // Set the duty cycle
+    TBCCR0 = 10000; // Aprox 10ms
     TBCCR1 = dutycycle;
-
-    // Set output mode to Reset/Set
     TBCCTL1 = OUTMOD_7;
 }
 
 void PWM_timer_undo(){
-    // Enable write access to port mapping registers
     PMAPKEYID = PMAPKEY;
-    P4MAP7 = PM_NONE; // Unmap TB0.1 from P4.7
-    PMAPKEYID = 0; // Disable write access to port mapping registers
+    P4MAP7 = PM_NONE;
+    PMAPKEYID = 0;
 
     // Reset Timer B0
     TBCTL = TBCLR;
@@ -270,8 +245,8 @@ void PWM_timer_undo(){
     TBCCR1 = 0;
     TBCCTL1 = 0;
 
-    // Set P4.7 as GPIO output
+    // Set P4.7 as output
     P4DIR |= BIT7;
-    P4SEL &= ~BIT7; // Ensure P4.7 is not selected for peripheral function
+    P4SEL &= ~BIT7;
     P4OUT &= ~BIT7;
 }
